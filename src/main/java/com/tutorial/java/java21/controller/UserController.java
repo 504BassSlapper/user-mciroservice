f7a7c223b6d0aae6bae09ee7ef7ac38583bc5221
504BassSlapper <504BassSlapper@gmail.com>
2023-10-01 15:55:21 +0200

    add restfull controller to get user

--- src/main/java/com/tutorial/java/java21/controller/UserController.java
+++ src/main/java/com/tutorial/java/java21/controller/UserController.java
@@ -1,7 +1,32 @@
 package com.tutorial.java.java21.controller;
 
-import org.springframework.stereotype.Controller;
+import com.tutorial.java.java21.entity.User;
+import com.tutorial.java.java21.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/get")
 public class UserController {
+    public static final Logger LOG =  LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user/{name}")
+    public ResponseEntity<User> getUser(@PathVariable String name) {
+        User user = userService.findUserByName(name);
+        if (user == null) {
+            LOG.warn("The user {} is not found!", name);
+            return new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
+        }
+        LOG.warn("The user {} is found!", name);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
